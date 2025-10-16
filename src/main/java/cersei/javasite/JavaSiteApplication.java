b510b4375d1c86b76ffd37f9f9edb17332ddbc5d
Cersei <bestkarmarussia@gmail.com>
2025-10-16 08:38:48 +0300

    Project genereted in idea

---
+++ src/main/java/cersei/javasite/JavaSiteApplication.java
@@ -0,0 +1,13 @@
+package cersei.javasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cersei/javasite/JavaSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package cersei.javasite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
