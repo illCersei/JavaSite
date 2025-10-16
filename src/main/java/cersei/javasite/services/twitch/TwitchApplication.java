118c5419fa01f38f888c9b9a9d50b6e74ccabfe3
Cersei <bestkarmarussia@gmail.com>
2025-10-16 08:51:31 +0300

    Skeleton

---
+++ src/main/java/cersei/javasite/services/twitch/TwitchApplication.java
@@ -0,0 +1,11 @@
+package cersei.javasite.services.twitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TwitchApplication.class, args);
+    }
+}
