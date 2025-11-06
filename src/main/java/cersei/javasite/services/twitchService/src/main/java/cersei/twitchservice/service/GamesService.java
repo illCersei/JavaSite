66e7ca22a10e5e906e1c74d5abe6b4d3d4bef503
Cersei <bestkarmarussia@gmail.com>
2025-11-06 09:08:28 +0300

    Logs in twitch service

--- src/main/java/cersei/javasite/services/twitchService/src/main/java/cersei/twitchservice/service/GamesService.java
+++ src/main/java/cersei/javasite/services/twitchService/src/main/java/cersei/twitchservice/service/GamesService.java
@@ -3,11 +3,12 @@ package cersei.twitchservice.service;
 import cersei.twitchservice.model.Games;
 import cersei.twitchservice.repository.GamesRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GamesService {
--- src/main/java/cersei/javasite/services/twitchService/src/main/java/cersei/twitchservice/service/ViewerService.java
+++ src/main/java/cersei/javasite/services/twitchService/src/main/java/cersei/twitchservice/service/ViewerService.java
@@ -3,11 +3,13 @@ package cersei.twitchservice.service;
 import cersei.twitchservice.model.Viewer;
 import cersei.twitchservice.repository.ViewerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ViewerService {
