702986f1eda3d23f191a355f6db1bfef21e93062
Cersei <bestkarmarussia@gmail.com>
2025-11-15 16:23:09 +0300

    Добавлена защита на тестовый сервис

---
+++ src/main/java/cersei/javasite/services/testApi/src/main/java/cersei/testapi/controller/Controller.java
@@ -0,0 +1,14 @@
+package cersei.testapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello World");
+    }
+}
