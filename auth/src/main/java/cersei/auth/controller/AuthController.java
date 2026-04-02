51aa751e3136bfbabe0d524cb721b6fdb14027e7
Cersei <bestkarmarussia@gmail.com>
2026-04-02 08:11:47 +0300

    Удалены ненужные импорты

--- auth/src/main/java/cersei/auth/controller/AuthController.java
+++ auth/src/main/java/cersei/auth/controller/AuthController.java
@@ -3,22 +3,14 @@ package cersei.auth.controller;
 import cersei.auth.dto.*;
 import cersei.auth.service.AuthService;
 import cersei.auth.service.RefreshTokenService;
-import cersei.common.error.errors.ApiErrorResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.common.Uuid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Ref;
 import java.util.UUID;
 
 @RestController
