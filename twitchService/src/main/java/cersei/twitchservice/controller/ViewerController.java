48f0483d123e2c3e8b71a8aab7084a547a52dd99
Cersei <bestkarmarussia@gmail.com>
2026-04-03 18:08:27 +0300

    Твич кэш исправление

--- twitchService/src/main/java/cersei/twitchservice/controller/ViewerController.java
+++ twitchService/src/main/java/cersei/twitchservice/controller/ViewerController.java
@@ -1,21 +1,10 @@
 package cersei.twitchservice.controller;
 
-import cersei.twitchservice.dto.PageViewerDto;
 import cersei.twitchservice.dto.ViewerDto;
 import cersei.twitchservice.service.ViewerService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,10 +21,7 @@ public class ViewerController {
     private final ViewerService viewerService;
 
     @GetMapping("/all")
-    @Cacheable(value = "ViewersCache", key = "'fixed'")
-    @Scheduled(fixedRate = 1000 * 60 * 60 * 3) // 3 h
     public List<ViewerDto> getAllViewers() {
-        log.info("Сделан запрос на вьюверов");
-        return viewerService.findMaxViewersByDay();
+        return viewerService.getCachedViewers();
     }
 }
--- twitchService/src/main/java/cersei/twitchservice/service/ViewerService.java
+++ twitchService/src/main/java/cersei/twitchservice/service/ViewerService.java
@@ -1,12 +1,12 @@
 package cersei.twitchservice.service;
 
 import cersei.twitchservice.dto.ViewerDto;
-import cersei.twitchservice.model.Viewer;
 import cersei.twitchservice.repository.ViewerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +17,20 @@ import java.util.List;
 public class ViewerService {
     private final ViewerRepository viewerRepository;
 
-    public List<ViewerDto> findMaxViewersByDay() {
+    @Cacheable(value = "ViewersCache", key = "'fixed'")
+    public List<ViewerDto> getCachedViewers() {
+        log.info("Сделан запрос на вьюверов (промах кэша, загрузка из БД)");
+        return loadMaxViewersByDay();
+    }
+
+    @Scheduled(fixedRate = 1000L * 60 * 60 * 3)
+    @CachePut(value = "ViewersCache", key = "'fixed'")
+    public List<ViewerDto> refreshViewersCache() {
+        log.info("Плановое обновление кэша зрителей");
+        return loadMaxViewersByDay();
+    }
+
+    private List<ViewerDto> loadMaxViewersByDay() {
         return viewerRepository.findMaxViewersGroupedByDay();
     }
 }
